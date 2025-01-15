da2e9592b8761606de6cd072b9dc89872f9d325c
SpyrosGavriil <spyrosgavriil01@gmail.com>
2025-01-15 19:46:34 +0200

    Added Controller Layer For User and Admin

---
+++ src/main/java/com/bloodmanagement/controller/AdminController.java
@@ -0,0 +1,43 @@
+package com.bloodmanagement.controller;
+
+import com.bloodmanagement.model.Admin;
+import com.bloodmanagement.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final AdminService adminService;
+
+    @Autowired
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @GetMapping
+    public List<Admin> getAllAdmins() {
+        return adminService.getAllAdmins();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Admin> getAdminById(@PathVariable Long id) {
+        return adminService.getAdminById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Admin createAdmin(@RequestBody Admin admin) {
+        return adminService.saveAdmin(admin);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAdmin(@PathVariable Long id) {
+        adminService.deleteAdmin(id);
+    }
+}
