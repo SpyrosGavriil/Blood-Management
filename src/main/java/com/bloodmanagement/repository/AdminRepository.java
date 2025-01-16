08568b8dc6b27a17563ae17c25e52b2b95093d7d
SpyrosGavriil <spyrosgavriil01@gmail.com>
2025-01-16 17:12:17 +0200

    Update User and Admin repository classes

--- src/main/java/com/bloodmanagement/repository/AdminRepository.java
+++ src/main/java/com/bloodmanagement/repository/AdminRepository.java
@@ -1,14 +1,18 @@
 package com.bloodmanagement.repository;
 
 import com.bloodmanagement.model.Admin;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface AdminRepository extends BaseUserRepository<Admin> {
+public interface AdminRepository extends JpaRepository<Admin, Integer> {
     List<Admin> findByDepartment(String department);
 
     Optional<Admin> findByEmployeeId(String employeeId);
+
+    Optional<Admin> findByUsername(String username);
 }
--- src/main/java/com/bloodmanagement/repository/UserRepository.java
+++ src/main/java/com/bloodmanagement/repository/UserRepository.java
@@ -1,15 +1,20 @@
 package com.bloodmanagement.repository;
 
 import com.bloodmanagement.model.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface UserRepository extends BaseUserRepository<User> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     List<User> findByBloodGroup(String bloodGroup);
 
     List<User> findByIsDonorTrue();
 
     List<User> findByEligibleToDonateTrue();
+
+    Optional<User> findByUsername(String username);
 }
