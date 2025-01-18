3c283e3b7b357e9a7b6b4eef6797f643b135274e
SpyrosGavriil <spyrosgavriil01@gmail.com>
2025-01-18 12:40:25 +0200

    Minor Change

--- src/main/java/com/bloodmanagement/repository/BloodBankRepository.java
+++ src/main/java/com/bloodmanagement/repository/BloodBankRepository.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.bloodmanagement.dto.BloodBankDTO;
 import com.bloodmanagement.model.BloodBank;
 
 @Repository
